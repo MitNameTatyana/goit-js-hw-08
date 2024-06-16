8432417c68582562022001c422f376d310f0727a
Tatyana Chuiko <chyikotatyana@gmail.com>
2024-06-16 21:45:47 +0200

    done hw8

--- js/task-1.js
+++
@@ -1,7 +0,0 @@
-const listElems = document.querySelectorAll('.item');
-console.log(`Number of categories: ${listElems.length}`);
-
-for (const elem of listElems) {
-  console.log(`Category: ${elem.firstElementChild.textContent}`);
-  console.log(`Elements: ${elem.querySelectorAll('li').length}`);
-}
